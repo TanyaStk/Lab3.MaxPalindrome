4879d1e61d7f18f9c8136d90888e15350f5ea1f3
TanyaStk <64659217+TanyaStk@users.noreply.github.com>
2020-09-23 23:12:32 +0300

    develop

--- src/main/java/com/project1/Main.java
+++ src/main/java/com/project1/Main.java
@@ -1,11 +1,49 @@
 package com.project1;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        try(Scanner scr = new Scanner(System.in)){
+    public static void main(String[] args) {
+        try (Scanner scr = new Scanner(System.in)) {
             System.out.println("Enter text:");
+            List<String> text = new ArrayList<>();
+            String nextLine = scr.nextLine();
+            if(nextLine.isEmpty()){
+                throw new IOException();
+            }
+            while (!nextLine.isEmpty()) {
+                text.add(nextLine);
+                nextLine = scr.nextLine();
+            }
+            System.out.println("Max palindrome is: " + findMaxPalindrome(text));
         }
+        catch(IOException ex){
+            System.out.println("Empty string.");
+        }
+    }
+
+    static String findMaxPalindrome(List<String> text) {
+        String maxPalindrome = "";
+        int maxSizeOfPalindrome = 0;
+        for (String line : text) {
+            StringTokenizer tokenizer = new StringTokenizer(line, " ,.");
+            while (tokenizer.hasMoreTokens()) {
+                String word = tokenizer.nextToken();
+                if (isPalindrome(word) && word.length() > maxSizeOfPalindrome) {
+                    maxPalindrome = word;
+                    maxSizeOfPalindrome = word.length();
+                }
+            }
+        }
+        return maxPalindrome;
+    }
+
+    static boolean isPalindrome(String word) {
+        return word.equalsIgnoreCase(
+                new StringBuilder(word).reverse().toString());
     }
 }
