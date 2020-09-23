bcd97570e515b35950c88ccd3615df4b86cae7bf
TanyaStk <64659217+TanyaStk@users.noreply.github.com>
2020-09-23 19:37:38 +0300

    Initial commit

---
+++ src/main/java/com/project1/Main.java
@@ -0,0 +1,11 @@
+package com.project1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        try(Scanner scr = new Scanner(System.in)){
+            System.out.println("Enter text:");
+        }
+    }
+}
